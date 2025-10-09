1e12c80cb30ef0af4c2831c0dea5d1c011cd186d
Eduardo Franceschi <eduardo.franceschi@databricks.com>
2025-10-09 15:26:27 -0300

    JAVA_HOME is now optional

--- compile.sh
+++ compile.sh
@@ -3,7 +3,7 @@
 # Script to compile Delta Sharing project with Java 17
 
 # Configure Java 17
-export JAVA_HOME=/opt/homebrew/opt/openjdk@17
+export JAVA_HOME="${JAVA_HOME:-/opt/homebrew/opt/openjdk@17}"
 export PATH="$JAVA_HOME/bin:$PATH"
 
 echo "🔨 Compiling Delta Sharing OnPrem..."
